63f6b47fcbc5618cdc9a72bb02491c0be7ae2b98
Julien MARTIN-PRIN <julienmartinprin@gmail.com>
2020-04-28 11:53:34 +0200

    Added necessary function

--- headers/inputparse.py
+++ headers/inputparse.py
@@ -2,5 +2,17 @@
 # -*- coding: utf-8 -*-
 # authors @Flexiboy
 
-class InputParse:
+from abc import ABC, abstractmethod
+import .input as inp
 
+class InputParse:
+	
+	def __init__(self, inp.input):
+		self.input = inp.input
+		self.parse = "" 
+
+	def execute():
+		pass
+
+	def parse(self.input):
+		pass
