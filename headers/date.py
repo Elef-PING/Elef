da875a5d1f46d584d1a1ae74514bccdfc59bc74c
LeoDPlouc <leo.lambert02@gmail.com>
2020-04-28 11:26:16 +0200

    Added save to date.py

--- headers/date.py
+++ headers/date.py
@@ -4,13 +4,23 @@
 # authors @LeoDPlouc
 
 import output as out 
-import datetime as dt
+from datetime import date
+import pickle as pck
+from io import FileIO as file
+
+CALENDAR_PATH = "../files/calender.cal"
 
 class Date:
-    def __init__(self, datetime, title, message):
+    def __init__(self, date = date, title = str, message = str):
         self.date = datetime
         self.title = title
         self.message = message
 
     def __str__(self):
         return "{0} : {1}, {2}".format(str(self.date), self.title, self.message)
+
+def save(dateObject = Date):
+    f = file(CALENDAR_PATH, "a")
+    pickler = pck.Pickler(f)
+    pickler.dump(dateObject)
+    f.close()
