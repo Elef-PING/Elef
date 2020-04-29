ed58e26f328fd4e2d20b6d91fb2e6fe9dcec433e
LeoDPlouc <leo.lambert02@gmail.com>
2020-04-29 13:28:02 +0200

    created getName in configure.py

--- headers/configure.py
+++ headers/configure.py
@@ -6,6 +6,8 @@ import json
 from io import FileIO as file
 
 CONFIGURE_PATH = "../files/.configure"
+NAME_KEY = "name"
+LANG_KEY = "lang"
 
 def getConfigure():
     cf = None
@@ -13,7 +15,7 @@ def getConfigure():
         f = file(CONFIGURE_PATH, "r")
         cf = json.load(f)
     except:
-        cf = {"name": None, "lang": None}
+        cf = {NAME_KEY: None, LANG_KEY: None}
     return cf
 
 def setConfigure(configure = dict):
@@ -21,6 +23,8 @@ def setConfigure(configure = dict):
     json.dump(configure, f)
 
 def getName():
-
+    cf = getConfigure()
+    return cf[NAME_KEY]
 
 def setName(name = str):
+    
