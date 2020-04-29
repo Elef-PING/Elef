d3e0f66e77474b046eb1edaed692fb6d80645f47
LeoDPlouc <leo.lambert02@gmail.com>
2020-04-29 13:23:05 +0200

    Created getConfigure in configure.py

--- headers/configure.py
+++ headers/configure.py
@@ -2,5 +2,20 @@
 # -*- coding: utf-8 -*-
 # authors @Flexiboy
 
-class Configure:
+import json
+from io import FileIO as file
 
+CONFIGURE_PATH = "../files/.configure"
+
+def getConfigure():
+    cf = None
+    try:
+        f = file(CONFIGURE_PATH, "r")
+        cf = json.load(f)
+    except:
+        cf = {"name": None, "lang": None}
+
+def getName():
+
+
+def setName(name = str):
