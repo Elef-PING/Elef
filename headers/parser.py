acdadb5b6d32c35ce00261d364750c240f22d6b6
Julien MARTIN-PRIN <julienmartinprin@gmail.com>
2020-05-05 12:20:59 +0200

    Modified the parser

--- headers/parser.py
+++ headers/parser.py
@@ -5,37 +5,67 @@
 from abc import ABC, abstractmethod
 from input import *
 
-PATH = ../files/keywords.xml
-LANG = "FR"
+APP_PATH = ../files/application_keywords.xml
+ACT_PATH = ../files/action_keywords.xml
+TIM_PATH = ../files/time_keywords.xml
+LANG = configure.getLang()
 
 class Parser(Input):
 
 	def __init__(self, inp):
 		self.input = inp
-		self.parsed = parser(self.inp)
-		self.keywords = []
+		self.application_keywords = []
+		self.action_keywords = []
+		self.time_keywords = []
 		
 		tmp = []
-		with open(PATH, "r") as keywordsfile:
+		with open(APP_PATH, "r") as keywordsfile:
 			for i in keywordsfile:
 				index1, index2 = i.find("kw"), i.find("lang")
-				if index2 == -1:
+				if index2 == -1 AND index1 != -1:
 					tmp.append(i.split("\"")[1])
-				elif index1 == -1:
+				elif index1 == -1 AND index2 != -1:
 					if i[12:13] == LANG:
 						tmp.append(i.split(">")[1].split("<")[0])
-						self.keywords.append(tmp)
+						self.application_keywords.append(tmp)
 						tmp = []
 				elif index1 == -1 && index2 == -1:
 					print("file error")
 
+		with open(ACT_PATH, "r") as keywordsfile:
+			for i in keywordsfile: 
+				index1, index2 = i.find("kw"), i.find("lang")
+				if index2 == -1 AND index1 != -1:
+					tmp.append(i.split("\"")[1]
+				elif index1 == -1 AND index2 != -1:
+					if i[12:13] == LANG:
+						tmp.append(i.split(">")[1].split("<")[0])
+						self.action_keywords.append(tmp)
+						tmp = []
+				elif index1 == -1 && index2 == -1:
+					print("file error")
+
+		with open(TIM_PATH, "r") as keywordsfile:
+			for i in keywordsfile:
+				index1, index2 = i.find("kw"), i.find("lang")
+				if index2 == -1 AND index1 != -1:
+					tmp.append(i.split("\"")[1]
+				elif index1 == -1 AND index2 != -1:
+					
+
+		self.parsed = parser(self.input, keywords)
+
 
 	def evaluate(self):
-		if parser(self) is not None:
+		if self.parsed is not None:
 			return 0
+		else:
+			return -1
 
-	def parser(self):
+	def parser(self, inp, keywords):
 		parsed = None
 		if isinstance(self, str):
+			
+		return parsed
 			
 
