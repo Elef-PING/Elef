56aab097fe8d9a4d1ea8935a08a312cbe40a07ec
LeoDPlouc <leo.lambert02@gmail.com>
2020-05-05 11:34:17 +0200

    Added execute un appKeyword

--- headers/appkeyword.py
+++ headers/appkeyword.py
@@ -2,8 +2,16 @@
 # -*- coding: utf-8 -*-
 # authors @Flexiboy
 
+import calendar
+
 class appKeyword():
     def __init__(self, app : str, func : str, args : list):
         self.app = app
         self.func = func
         self.args = args
+
+    def execute(self):
+        if self.app == "calendar":
+            if self.func == "create":
+                event = calendar.Date(self.args[0], self.args[1], self.args[2])
+                event.save()
